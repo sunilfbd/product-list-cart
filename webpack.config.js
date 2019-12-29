3ce1494a836b83aa7a9fdadb430e13140143a965
Sunil Ahuja <sunil.ahuja@akqa.com>
2019-12-29 16:49:22 +0530

    upgraded versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 var webpack = require("webpack");
 var path = require("path");
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-	entry: [SRC_DIR+"/app/index.js", SRC_DIR+"/sass/main.scss"],
+	entry: [SRC_DIR + "/app/index.js", SRC_DIR + "/sass/main.scss"],
 	output: {
-		path: DIST_DIR+"/app",
+		path: DIST_DIR + "/app",
 		filename: "bundle.js",
 		publicPath: "/app/"
 	},
@@ -17,28 +17,24 @@ var config = {
 	},
 	module: {
 		rules: [{
-			test: /\.js?/,
-			include: SRC_DIR,
-			loader: "babel-loader", 
-			query: {
-				presets: ["react","es2015","stage-2"],
-				plugins: ["transform-es2015-modules-amd"]
-			}
-		},
-		{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract({
-				fallback: 'style-loader',
-				use: ['css-loader', 'sass-loader']
-			})
-		}]
+				test: /\.js?/,
+				include: SRC_DIR,
+				loader: "babel-loader",
+				query: {
+					presets: ["react", "es2015", "stage-2"],
+					plugins: ["transform-es2015-modules-amd"]
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+		]
 	},
 	devServer: {
 		historyApiFallback: true
 	},
-	plugins: [
-		new ExtractTextPlugin('main.css'),
-	],
+	plugins: [new MiniCssExtractPlugin()],
 	devtool: 'cheap-module-eval-source-map'
 };
 
